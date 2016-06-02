f1d71a31e228817e2969de4c6fa8e35bd18f958b
Samuel Hibbard <hibbards01@gmail.com>
2016-06-02 13:10:46 -0600

    Almost done

--- naive_bayes.py
+++ naive_bayes.py
@@ -12,6 +12,10 @@
 from credentials import * # This is the file that has the credentials to
                           # connect to the database.
 import mysql.connector
+from operator import itemgetter
+total = 20000
+suicideProb = 13 / 100000
+regularProb = 1 - suicideProb
 
 #####################################################################
 # getSuicide
@@ -19,20 +23,20 @@ import mysql.connector
 #####################################################################
 def getSuicide(cursor):
     cursor.execute('''
-            SELECT * FROM keywords_suicidal WHERE total > 100 ORDER BY total DESC
-        ''')
+            SELECT * FROM keywords_suicidal WHERE total > %s ORDER BY total DESC
+        ''', (total,))
 
     array = dict(cursor.fetchall())
     keywords = { key.decode("utf-8"):value for key,value in array.items() }
 
     cursor.execute('''
-            SELECT COUNT(*) FROM keywords_suicidal WHERE total > 100 ORDER BY total DESC
+            SELECT COUNT(*) FROM keywords_suicidal
         ''')
 
     keywords['totalKeywords'] = int(cursor.fetchall()[0][0])
 
     cursor.execute('''
-            SELECT SUM(total) FROM keywords_suicidal WHERE total > 100
+            SELECT SUM(total) FROM keywords_suicidal
         ''')
 
     keywords['totalUseOfKeywords'] = int(cursor.fetchall()[0][0])
@@ -45,20 +49,20 @@ def getSuicide(cursor):
 #####################################################################
 def getRegular(cursor):
     cursor.execute('''
-            SELECT * FROM keywords_regular WHERE total > 100 ORDER BY total DESC
-        ''')
+            SELECT * FROM keywords_regular WHERE total > %s ORDER BY total DESC
+        ''', (total,))
 
     array = dict(cursor.fetchall())
     keywords = { key.decode("utf-8"):value for key,value in array.items() }
 
     cursor.execute('''
-            SELECT COUNT(*) FROM keywords_regular WHERE total > 100 ORDER BY total DESC
+            SELECT COUNT(*) FROM keywords_regular
         ''')
 
     keywords['totalKeywords'] = int(cursor.fetchall()[0][0])
 
     cursor.execute('''
-            SELECT SUM(total) FROM keywords_regular WHERE total > 100
+            SELECT SUM(total) FROM keywords_regular
         ''')
 
     keywords['totalUseOfKeywords'] = int(cursor.fetchall()[0][0])
@@ -66,13 +70,64 @@ def getRegular(cursor):
     return keywords
 
 #####################################################################
+# getMax
+#####################################################################
+def getMax(d):
+    v = list(d.values())
+    k = list(d.keys())
+
+    return k[v.index(max(v))]
+
+#####################################################################
 # createTable
 #   This will create the probability tables.
 #####################################################################
 def createTable(suicidal, regular):
-    # Loop through the keywords and add the probabilities
+    keywords = {}
+    suicidalTotalUse = suicidal.pop('totalUseOfKeywords', None)
+    suicidalTotal = suicidal.pop('totalKeywords', None)
+    regularTotalUse = regular.pop('totalUseOfKeywords', None)
+    regularTotal = regular.pop('totalKeywords', None)
+    total = suicidalTotalUse + regularTotalUse
+
     for key, value in suicidal.items():
-        pass
+        keywords[key] = value
+
+    for key, value in regular.items():
+        if key not in keywords:
+            keywords[key] = value
+        else:
+            keywords[key] += value
+
+    for key,value in keywords.items():
+        try:
+            suicidal[key]
+        except:
+            suicidal[key] = 0
+
+        probKeywordGivenSuicide = suicidal[key] / suicidalTotalUse
+        probKeyword = value / total
+
+        suicidal[key] = probKeywordGivenSuicide * suicideProb / probKeyword
+
+        try:
+            regular[key]
+        except:
+            regular[key] = 0
+
+        probKeywordGivenRegular = regular[key] / regularTotalUse
+        regular[key] = probKeywordGivenRegular * regularProb / probKeyword
+
+    # key = getMax(suicidal)
+
+    # print("key: ", key, " value: ", suicidal[key])
+
+    # key = getMax(regular)
+
+    # print("key: ", key, " value: ", regular[key])
+    print(sorted(suicidal.items(), key=itemgetter(1)))
+    print(sorted(regular.items(), key=itemgetter(1)))
+
 
 #####################################################################
 # main
